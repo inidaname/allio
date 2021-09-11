2745092f267bd3c76db17269c85e3166b79b2e1e
Hassan Sani <saniyhassan@gmail.com>
2021-09-11 01:06:28 +0100

    Create header and footer layout component

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent, ReactElement } from "react";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+
+const Footer: FunctionComponent = (): ReactElement => {
+  return (
+    <footer>
+
+    </footer>
+  );
+};
+
+export default Footer;
